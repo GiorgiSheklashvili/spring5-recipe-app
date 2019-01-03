dd0ef5875bdc672324d755a0aacfd67c38902099
Giorgi Sheklashvili <sheklashviligiorgi@gmail.com>
2019-01-03 21:57:43 +0200

    one-to-one between ingredient and unit of measure added(assignment)

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -14,7 +14,8 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
     //private UnitOfMeasure uom;
 
     @ManyToOne
@@ -50,5 +51,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
